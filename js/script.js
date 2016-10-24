d2a9e28a66e72595d56fc62d2b4c31dcab9652b8
José Becerra <antonio.becerra@grupomedios.com>
2016-10-24 17:38:39 -0500

    radioactivate

---
+++ js/script.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+	$("#carrusel").owlCarousel({
+		 singleItem:true,
+		 pagination:false,
+		 navigation:true,
+		 navigationText:false,
+		 transitionStyle : "fadeUp"
+	});
+});
